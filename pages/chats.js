c5909a0b0b7cee47d99368a5994d430bfaa3d5d4
aaronalerander <aaronalexander.inc@gmail.com>
2021-12-29 14:37:48 -0500

    layout better

--- pages/chats.js
+++ pages/chats.js
@@ -12,6 +12,18 @@ const MessageFormSocial = dynamic(() =>
   import("react-chat-engine").then((module) => module.MessageFormSocial)
 );
 
+const ChatEngineWrapper = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngineWrapper)
+);
+
+const ChatSocket = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatSocket)
+);
+
+const ChatFeed = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatFeed)
+);
+
 export default function Chats() {
   const { username, secret } = useContext(Context);
   const [showChat, setShowChat] = useState(false);
@@ -30,16 +42,30 @@ export default function Chats() {
   if (!showChat) return <div />;
 
   return (
-    <div className="background">
-      <div className="shadow">
-        <ChatEngine
-          height="calc(100vh - 200px)"
+    <div className="background-chat">
+      <ChatEngineWrapper>
+        <ChatSocket
           projectID="27e66807-2beb-4656-9292-e6e821608ab9"
-          userName={username}
-          userSecret={secret}
-          renderNewMessageForm={() => <MessageFormSocial />}
+          chatID="82737"
+          chatAccessKey="ca-945fa971-2859-4787-bda4-f29fc88946c4"
+          senderUsername={username}
         />
-      </div>
+        <div className="flex-box">
+          <h1 style={{ marginRight: "20px" }}>Project x</h1>
+          <button type="submit" className="add-chat-button">
+            Click to add a new chat
+          </button>
+        </div>
+
+        <div className="flex-box">
+          <div className="chat-container">
+            <ChatFeed
+              activeChat="82737"
+              renderNewMessageForm={() => <MessageFormSocial />}
+            />
+          </div>
+        </div>
+      </ChatEngineWrapper>
     </div>
   );
 }
